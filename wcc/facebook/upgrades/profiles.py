bf96a112f177bb28130645dc6a8be5e7c2d7fb13
Khairul Anwar (kulll) <anwarbaik88@gmail.com>
2013-11-05 13:33:14 +0800

    add upgrade profile for wccfacebookportlet

--- wcc/facebook/upgrades/profiles.py
+++ wcc/facebook/upgrades/profiles.py
@@ -1,3 +1,8 @@
 from collective.grok import gs
 
 # -*- extra stuff goes here -*- 
+
+gs.profile(name=u'to2',
+           title=u'Upgrade wcc.facebook to 2',
+           description='',
+           directory='profiles/to2')
